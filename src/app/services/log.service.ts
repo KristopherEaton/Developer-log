19e272f8abf7487fa464863658f0bcba30ba7418
Kristopher Eaton <kristophereaton12@gmail.com>
2021-03-10 15:43:59 -0700

    first commit

---
+++ src/app/services/log.service.ts
@@ -0,0 +1,76 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Observable } from 'rxjs';
+import { of } from 'rxjs';
+import { Log } from '../models/Log';
+
+@Injectable()
+export class LogService {
+  logs: Log[];
+
+  private logSource = new BehaviorSubject<Log>({ id: null, text: null, date: null });
+  selectedLog = this.logSource.asObservable();
+
+  private stateSource = new BehaviorSubject<boolean>(true);
+  stateClear = this.stateSource.asObservable();
+
+  constructor() {
+
+    // this.logs = [
+    //   {id: '1', text: 'Generated components', date: new Date('12/26/2017 12:54:23' )},
+    //   {id: '2', text: 'added Bootstrap', date: new Date('12/26/2017 19:54:23' )},
+    //   {id: '3', text: 'added logs component', date: new Date('12/27/2017 12:54:23' )},
+    // ]
+    this.logs = [];
+  }
+
+  getLogs(): Observable<Log[]> {
+    if (localStorage.getItem('logs') === null) {
+      this.logs = [];
+    } else {
+      this.logs = JSON.parse(localStorage.getItem('logs'));
+    }
+
+    return of(this.logs.sort((a, b) => {
+      return b.date = a.date;
+    }))
+  }
+
+  setFormLog(log: Log) {
+    this.logSource.next(log);
+  }
+
+  addLog(log: Log) {
+    this.logs.unshift(log);
+
+    localStorage.setItem('logs', JSON.stringify(this.logs));
+  }
+
+  updateLog(log: Log) {
+    this.logs.forEach((cur, index) => {
+      if (log.id === cur.id) {
+        this.logs.splice(index, 1);
+      }
+    });
+    this.logs.unshift(log);
+    localStorage.setItem('logs', JSON.stringify(this.logs));
+  }
+
+  deleteLog(log: Log) {
+    this.logs.forEach((cur, index) => {
+      if (log.id === cur.id) {
+        this.logs.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('logs', JSON.stringify(this.logs));
+  }
+
+  clearState() {
+    this.stateSource.next(true);
+  }
+
+
+
+
+}
